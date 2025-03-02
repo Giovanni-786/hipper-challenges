7f3efd6176dfe87e62403146cb5c031b3c23d40d
Giovanni Sena <giovanni.lima786@gmail.com>
2025-03-02 12:21:06 -0300

    inserir card de preview

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { ChallengePage } from '../ChallengePage/ChallengePage'
-import CardNotification from '../CardNotification/CardNotification'
-// import CardNotificationPreview from '../CardNotificationPreview/CardNotificationPreview'
+// import CardNotification from '../CardNotification/CardNotification'
+import CardNotificationPreview from '../CardNotificationPreview/CardNotificationPreview'
 
 function Welcome() {
   const [nextStep, setNextStep] = useState(false)
@@ -9,8 +9,8 @@ function Welcome() {
     <article className="flex min-h-screen w-full justify-center bg-gradient-to-br from-[#8ee3ef] via-white to-[#8ee3ef]">
       <div className="mx-auto my-0 mt-4 text-5xl">
         {!nextStep ? (
-          <CardNotification setNextStep={setNextStep} />
-          // <CardNotificationPreview />
+          // <CardNotification setNextStep={setNextStep} />
+          <CardNotificationPreview />
         ) : (
           <ChallengePage setNextStep={setNextStep} />
         )}
